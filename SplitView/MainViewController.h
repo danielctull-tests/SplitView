bba8992b8cd7a5aee0b971fce46fd10d1d13bd37
Mike Abdullah <me@mikeabdullah.net>
2015-11-14 14:48:10 +0000

    Custom class for container VC
    Helpful to poke around a bit while debugging

---
+++ SplitView/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SplitView
+//
+//  Created by Mike on 14/11/2015.
+//  Copyright © 2015 Karelia Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
